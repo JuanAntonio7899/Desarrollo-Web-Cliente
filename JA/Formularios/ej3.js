37701c154a7e7f5692fb6118171d8789b59ab6ca
Juan Antonio <150024885+JuanAntonio7899@users.noreply.github.com>
2024-11-25 10:50:12 +0100

    Update ej3.js

--- JA/Formularios/ej3.js
+++ JA/Formularios/ej3.js
@@ -1,35 +1,24 @@
-window.onload = function ()  {
-    let formu = document.querySelector("form");
-    let furbito = formu.elements[0];
-    let cine = formu.elements[1];
-    let jueguitos = formu.elements[2]; 
-
-    let etiquetas = document.querySelectorAll("label");
-
-   
-
-    let texto = formu.elements[3];
-    let boton = formu.elements[4];
+window.onload = function () {
+    var formu = document.querySelector("form");
+    var checkboxes = formu.querySelectorAll('input[type="checkbox"]');
+    var etiquetas = document.querySelectorAll("label");
+    var texto = formu.querySelector("textarea");
+    var boton = formu.querySelector("button");
 
     boton.onclick = function () {
-        for (const eti of etiquetas) {
+        // Reiniciar el contenido del textarea
+        texto.value = "";
 
-            if (furbito.checked == true) {
-
-                texto.value = eti.textContent;
-                
-            }else if (cine.checked == true){
-                texto.value += " " + eti.textContent;
-    
-            }else{
-                texto.value += " " + eti.textContent;
+        // Usar for...of para recorrer checkboxes y etiquetas simultáneamente
+        for (var checkbox of checkboxes) {
+            if (checkbox.checked) {
+                // Buscar la etiqueta asociada al checkbox
+                for (var etiqueta of etiquetas) {
+                    if (etiqueta.contains(checkbox)) {
+                        texto.value += etiqueta.textContent + "\n";
+                    }
+                }
             }
-        
-        
         }
-
-        
-    }
-    
-    
-}
+    };
+};
