403ecda3211cc7403e593b4de1af15b6d3e7ae5a
JuanAntonio7899 <jagzl7899@gmail.com>
2024-11-25 12:13:26 +0100

    ej3 terminado

--- JA/Formularios/ej3.js
+++ JA/Formularios/ej3.js
@@ -1,35 +1,53 @@
-window.onload = function ()  {
-    let formu = document.querySelector("form");
-    let furbito = formu.elements[0];
-    let cine = formu.elements[1];
-    let jueguitos = formu.elements[2]; 
+/**
+ * Crea un formulario con varios checkbox (pej, aficiones), un textarea y un boton.
+ * Implementa un programa que, tras pulsar el boton se muestre dentro del textarea todas
+ * las opciones marcadas.
+ * 
+ * b) Mejora el apartado anterior quitando el botón. Ahora cada vez que se marque un
+ * checkbox, se añadirá al contenido del textarea. Si se desmarca, hay que quitarlo.
+ */
 
-    let etiquetas = document.querySelectorAll("label");
+window.onload = function(){
 
-   
-
-    let texto = formu.elements[3];
-    let boton = formu.elements[4];
-
-    boton.onclick = function () {
-        for (const eti of etiquetas) {
-
-            if (furbito.checked == true) {
-
-                texto.value = eti.textContent;
-                
-            }else if (cine.checked == true){
-                texto.value += " " + eti.textContent;
     
-            }else{
-                texto.value += " " + eti.textContent;
-            }
-        
-        
-        }
 
-        
+    // Guardo el formulario
+    let formu = document.forms[0];
+    let aficiones = [];
+    
+    for (let i = 0; i < formu.length; i++) {
+        if (formu[i].type == "checkbox") {
+            
+            aficiones.push(formu[i]);
+        }
     }
     
-    
+    // Guardo el textarea
+    let textArea = document.querySelector("textarea");
+    // Guardo el boton
+    let boton = formu.elements[formu.length-1];
+
+    // APARTADO A
+    // boton.onclick = function(){
+    //    
+        
+    //     let textito = "";
+    //     for (let i = 0; i < aficiones.length; i++) {
+    //         console.log("Entra for");
+    //         if (aficiones[i].checked){
+    //             console.log("Entra en checked");
+    //             textito += aficiones[i].value+"\n";
+    //         }
+    //     }
+    //     textArea.value = textito;
+    // }
+
+    // APARTADO B
+    for (let i = 0; i < aficiones.length; i++) {
+        aficiones[i].onclick = function(){
+            if(aficiones[i].checked){
+                textArea.value += aficiones[i].value+"\n";
+            } else textArea.value = textArea.value.replace(aficiones[i].value+"\n","");
+        }
+    }
 }
